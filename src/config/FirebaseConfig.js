8050b174f69fd75140fa59b3f594a59da9caab9c
Bhavyasri-P <161433222+Bhavyasri-P@users.noreply.github.com>
2024-03-26 20:22:03 +0530

    Update FirebaseConfig.js

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdxNSVj_fhY5Irej7pGgBmbiLzwqLVSUk",
-  authDomain: "registration-form-694e2.firebaseapp.com",
-  databaseURL: "https://registration-form-694e2-default-rtdb.firebaseio.com",
-  projectId: "registration-form-694e2",
-  storageBucket: "registration-form-694e2.appspot.com",
-  messagingSenderId: "521258671770",
-  appId: "1:521258671770:web:8ce8a68ec814e839c50923",
-  measurementId: "G-K1BV1144XJ"
+  apiKey: "AIzaSyCJ6ppWepPoZdJozAwlTvLBb8YednjAxd4",
+  authDomain: "learnifyme-c5965-c02c3.firebaseapp.com",
+  databaseURL: "https://learnifyme-c5965-default-rtdb.firebaseio.com",
+  projectId: "learnifyme-c5965",
+  storageBucket: "learnifyme-c5965.appspot.com",
+  messagingSenderId: "362826558856",
+  appId: "1:362826558856:web:c967a560ca6c9033771e3d",
+  measurementId: "G-RDZMDMT3BE"
 };
 
 // Initialize Firebase
